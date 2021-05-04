c6e73f81e1ffc18f25d99714789cfd3d20b1af04
root <root@DESKTOP-BDR56UD.localdomain>
2021-05-03 18:53:40 -0700

    developing fasta to vcf functionality

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ function testAlignment() {
   var numSamples = 3;
   var snps = {};
   var indels = {};
+  var header_str = '';
+
   //pairwise align each sample to reference, collecting snps
   for (var sampleNum=0; sampleNum < numSamples; sampleNum++) {
     header_str += ' Sample' + sampleNum;
@@ -43,38 +45,38 @@ function testAlignment() {
     console.log(currAlignment);
 
     for (var i = 0; i < alnRef.length; i++) {
-      'AC-GG-T'
-       01 23 4
-       6
-      refPos = i - alnRef.substring(0, i+1).replace('-','').length;
-      
-      console.log(alnRef[i] + alnQuery[i])
+      var refPos = alnRef.join('').substring(0, i+1).replace('-','').length-1;
       if (alnRef[i] != alnQuery[i] && alnRef[i] != '-' && alnQuery[i] != '-') {
         if (!(i in snps)) {
-          snps[i] = {};
-          snps[i].genotypes = new Array(numSamples).fill(0);
-          snps[i].altAlleles = [];
+          snps[refPos] = {};
+          snps[refPos].genotypes = new Array(numSamples).fill(0);
+          snps[refPos].altAlleles = [];
+          snps[refPos].refAllele = alnRef[i];
         }
-        if (! (snps[i].altAlleles.includes(alnQuery[i])) ) { // if the alternate allele hasn't been seen yet, add it to the list
-          snps[i].altAlleles.push(alnQuery[i]);
+        if (! (snps[refPos].altAlleles.includes(alnQuery[i])) ) { // if the alternate allele hasn't been seen yet, add it to the list
+          snps[refPos].altAlleles.push(alnQuery[i]);
         }
-        snps[i].genotypes[sampleNum] = snps[i].altAlleles.indexOf(alnQuery[i]) + 1; // store the allele number per vcf specification
+        snps[refPos].genotypes[sampleNum] = snps[refPos].altAlleles.indexOf(alnQuery[i]) + 1; // store the allele number per vcf specification
       }
     }  
   }
 
   // save VCF file
+  console.log(snps)
   console.log('##fileformat=VCFv4.2');
   console.log(header_str);
   for (const pos in snps) {
-    idList = ''
-    genotypes = ''
-    refAllele = 
-    vcf_line = refName + '\t' + (pos+1) + '\t' + idList + '\t' + refAllele + '\t' + altAlleles + '\t.\t.\t.\tGT\t' + genotypes;
-  }  
-  for (var sampleNum=0; sampleNum < numSamples; sampleNum++) {
+    var idList = [];
+    var refAllele = snps[pos].refAllele;
+    var altAlleles = snps[pos].altAlleles;
+    for (var i; i < altAlleles.length; i++) {
+      idList.push(refAllele+pos+altAlleles[i])
+    }
+    var vcf_line = 'REFERENCE' + '\t' + (pos*1+1) + '\t' + idList.join(',') + '\t' + refAllele + '\t' + altAlleles.join(',') + '\t.\t.\t.\tGT\t' + snps[pos].genotypes.join(',');
+    console.log(vcf_line);
+  }
 
-    NC_045512v2	1440	G1440A	G	A	.	.	AC=482,9;AN=39342	GT	1	1	1	1	1
+  //  NC_045512v2	1440	G1440A	G	A	.	.	AC=482,9;AN=39342	GT	1	1	1	1	1
 
   
 //  alignPairwise()
